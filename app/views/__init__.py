4fe2c2d822cb459fc85bbef6d1a32ae117a9226e
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-07-09 12:44:51 -0300

    final config app

---
+++ app/views/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+
+def init_app(app: Flask):
+    # registrar as blueprints
+    ...
+    
---
+++ config.py
@@ -0,0 +1,22 @@
+from environs import Env
+
+env = Env()
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ConfigDevelopment(Config):
+    SQLALCHEMY_DATABASE_URI = env("URI_DEV_DB")
+    JWT_SECRET_KEY = env("JWT_SECRET_KEY")
+
+class ConfigProduction(Config):
+    ...
+
+class ConfigTest(Config):
+    ...
+
+selector_config = {
+    "development": ConfigDevelopment,
+    "production": ConfigProduction,
+    "test": ConfigTest
+}
