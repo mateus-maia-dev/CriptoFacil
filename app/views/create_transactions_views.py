9a82422088b5a3f4388d0cff31b88586ad318f44
Joao Manoel Ribas <joaoribas35@hotmail.com>
2021-07-12 14:56:31 -0300

    initial config

--- app/views/create_transactions_views.py
+++ app/views/create_transactions_views.py
@@ -2,8 +2,10 @@ from flask import Blueprint, request, current_app, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.transactions_model import Transaction
 from http import HTTPStatus
+import ipdb
 
 transactions = Blueprint("transactions", __name__, url_prefix="/api")
+
 
 @transactions.route("/transactions/register", methods=["POST"])
 @jwt_required()
@@ -12,7 +14,7 @@ def create_user():
     session = current_app.db.session
     user_id = get_jwt_identity()
 
-    print(body, user_id)
+    ipdb.set_trace()
 
     date = body.get("date")
     type = body.get("type")
@@ -25,9 +27,11 @@ def create_user():
     quantity = body.get("quantity")
     foreign_exch = body.get("foreign_exch")
 
-    new_transaction = Transaction(date=date, type=type, coin=coin, fiat=fiat, price_per_coin=price_per_coin, avg_price_brl=avg_price_brl, avg_price_usd=avg_price_usd, net_quantity=net_quantity, quantity=quantity, foreign_exch=foreign_exch, user_id=user_id)
+    # new_transaction = Transaction(date=date, type=type, coin=coin, fiat=fiat, price_per_coin=price_per_coin, avg_price_brl=avg_price_brl, avg_price_usd=avg_price_usd, net_quantity=net_quantity, quantity=quantity, foreign_exch=foreign_exch, user_id=user_id)
 
-    session.add(new_transaction)
-    session.commit()
+    # session.add(new_transaction)
+    # session.commit()
 
-    return new_transaction.serialized(), HTTPStatus.CREATED
+    # return new_transaction.serialized(), HTTPStatus.CREATED
+
+    return ""
