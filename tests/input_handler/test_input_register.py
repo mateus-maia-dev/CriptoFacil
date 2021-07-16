106a800dd5654a83866472d19757a2b1c970a2ff
mateusmaiaf <eng.fonsecamateus@gmail.com>
2021-07-16 10:14:06 -0300

    first try test

--- tests/input_handler/test_input_register.py
+++ tests/input_handler/test_input_register.py
@@ -1,6 +1,14 @@
+import pytest
+
+
 def inputs(num):
-    return num
+    return num + 411
 
 
 def test_para_testar_os_dados_para_registro():
     assert inputs(41) == 452
+
+
+def test_soma_411_palavra():
+    with pytest.raises(TypeError):
+        inputs("mateus")
