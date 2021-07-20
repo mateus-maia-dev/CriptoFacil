2cf37f3de7ffb467a57464481928b76332980b09
mateusmaiaf <eng.fonsecamateus@gmail.com>
2021-07-20 09:58:42 -0300

    validating fields in post methods

--- app/services/helper.py
+++ app/services/helper.py
@@ -7,6 +7,22 @@ def init_app(model) -> None:
     current_app.db.session.commit()
 
 
+def validate_fields(body: dict, required_fields: list):
+    keys = body.keys()
+
+    missing_fields = [key for key in required_fields if key not in keys]
+
+    if missing_fields:
+        raise KeyError(
+            {
+                'error': {
+                    'required_fields': required_fields,
+                    'missing_fields': missing_fields,
+                }
+            }
+        )
+
+
 def verify_received_keys_from_create_transactions(body: dict):
     required_fields = [
         'date',
@@ -18,24 +34,16 @@ def verify_received_keys_from_create_transactions(body: dict):
         'foreign_exch',
     ]
 
-    keys = body.keys()
-
-    missing_fields = [key for key in required_fields if key not in keys]
-    set_trace()
-    if missing_fields:
-        raise KeyError(
-            {
-                "error": {
-                    "required_field": required_fields,
-                    "missing_fields": list(missing_fields),
-                }
-            }
-        )
+    validate_fields(body, required_fields)
 
 
-def verify_received_keys_from_create_user():
-    ...
+def verify_received_keys_from_create_user(body: dict):
+    required_fields = ['name', 'email', 'last_name', 'password']
+
+    validate_fields(body, required_fields)
 
 
-def verify_received_keys_from_login():
-    ...
+def verify_received_keys_from_login(body: dict):
+    required_fields = ['email', 'password']
+
+    validate_fields(body, required_fields)
